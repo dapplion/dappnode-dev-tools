b91765517a812c06078d0176bae54231683fc086
dapplion <35266934+dapplion@users.noreply.github.com>
2019-01-28 14:40:01 +0700

    add echo

--- devtools.sh
+++ devtools.sh
@@ -14,6 +14,7 @@ DNP=\$1
 docker-compose -f ${DAPPNODE_SRC}/docker-compose-\${DNP}.yml up -d
 EOF
 chmod +x $UP
+echo "Added command 'up' to $UP"
 
 
 LOG=/usr/bin/log
@@ -23,6 +24,7 @@ DNP=\$1
 docker logs DAppNodeCore-\${DNP}.dnp.dappnode.eth -f
 EOF
 chmod +x $LOG
+echo "Added command 'log' to $LOG"
 
 
 INTO=/usr/bin/into
@@ -32,6 +34,7 @@ DNP=\$1
 docker exec -it DAppNodeCore-\${DNP}.dnp.dappnode.eth sh
 EOF
 chmod +x $INTO
+echo "Added command 'into' to $INTO"
 
 
 RESTORE=/usr/bin/restore
@@ -41,21 +44,25 @@ sudo rm  /usr/src/dappnode/docker-compose*.yml
 wget -O - https://github.com/dappnode/DAppNode/releases/download/v0.1.1/dappnode_install.sh | sudo bash
 EOF
 chmod +x $RESTORE
+echo "Added command 'restore' to $RESTORE"
 
 
 TO_VER=/usr/bin/toVer
 cat > $TO_VER <<EOF
 #!/bin/bash
-REPO=$1 # lowercase, i.e. dappmanager
-BRANCH=${2:-dev}
+REPO=\$1 # lowercase, i.e. dappmanager
+BRANCH=\${2:-dev}
 REPO_DIR="${DAPPNODE_SRC}/DNP_\$(echo \$REPO | awk '{print toupper(\$0)}')"
 sudo rm -rf \$REPO_DIR
-sudo git clone https://github.com/dappnode/\${REPO_DIR}.git -b \$BRANCH ${DAPPNODE_SRC}
+mkdir -p \$REPO_DIR
+sudo git clone https://github.com/dappnode/\${REPO_DIR}.git -b \$BRANCH \$REPO_DIR
 cd \$REPO_DIR
 docker-compose -f \${REPO_DIR}/docker-compose*.yml build
 up \$REPO
+log \$REPO
 EOF
 chmod +x $TO_VER
+echo "Added command 'toVer' to $TO_VER"
 
 
 #################
